41a55a7378de0b9b83f8c229f0e7480fca8ca249
oyerohabib <oyerohabib@yahoo.com>
2021-08-07 15:53:09 +0900

    modified number guesser game to implement functions

---
+++ modified_number_guesser.py
@@ -0,0 +1,61 @@
+import random
+
+top_of_range = 0
+random_number = 0
+guesses = 0
+
+def guessANumber():
+  global top_of_range
+  top_of_range = input("Type a number? ")
+
+  if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+      print("please type a number greater than 0 next time")
+      quit()
+      
+  else:
+    print("please type a number next time")
+    quit()
+    
+  global random_number
+  random_number = random.randint(0, top_of_range)
+    
+guessANumber()
+
+def confirm():
+  user_input = input("Do you still want to play, enter yes or no? \n").lower()
+  if user_input == "yes":
+    guessANumber()
+    playGame()
+  else:
+    quit()
+
+def playGame():
+  
+  while True:
+    global guesses
+    guesses += 1
+    user_guess = input("make a guess? ")
+    
+    if user_guess.isdigit():
+      user_guess = int(user_guess)
+      
+    else:
+      print("please type a number next time")
+      continue
+    
+    if user_guess == random_number:
+      print("you got it.")
+      print("you got it in", guesses, "guesses.\n")
+      confirm()
+      break
+    elif user_guess > random_number:
+      print("your guess is higher than the number")
+    else:
+      print("your guess is lower than the number")
+      
+playGame()
+
+confirm()
